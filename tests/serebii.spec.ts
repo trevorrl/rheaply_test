95c4bcbcd6d356b0714ce6d21b6c4d27d726f76c
Trevor Lemeron <trevorrl@me.com>
2022-07-09 15:51:56 -0400

    test 3 WIP, switching computers

--- tests/serebii.spec.ts
+++ tests/serebii.spec.ts
@@ -23,4 +23,25 @@ test.describe('Serebii Example Tests', () => {
     // assert that we are on the POTW page
     await expect(page.locator('title')).toContainText('Pokémon of the Week');
   })
+
+  test('navigate to Forums', async ({ page }) => {
+    // // find and click the forums link in the header
+    // await page.locator('text=Forums').first().click();
+    // // assert that the page has forums in the URL (using regex)
+    // await expect(page).toHaveURL('https://forums.serebii.net/');
+
+    const [forums] = await Promise.all([
+        page.waitForEvent('page'),
+        page.locator('text=Forums').first().click()
+    ])
+    await expect(forums).toHaveURL('https://forums.serebii.net/');
+  })
 })
+
+// Get page after a specific action (e.g. clicking a link)
+// const [newPage] = await Promise.all([
+//     context.waitForEvent('page'),
+//     page.click('a[target="_blank"]') // Opens a new tab
+//   ])
+//   await newPage.waitForLoadState();
+//   console.log(await newPage.title());
