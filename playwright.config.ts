b896d8dc66616cddcee7b8a741dc11462e521b2d
Trevor Lemeron <trevorrl@me.com>
2022-07-09 14:29:59 -0400

    second test done, output dir and config setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 50 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 10000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -40,6 +40,8 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    /* record video on failure */
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -95,7 +97,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'test-results',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
--- tests/serebii.spec.ts
+++ tests/serebii.spec.ts
@@ -11,7 +11,16 @@ test.describe('Serebii Example Tests', () => {
     await page.locator('text=Gen VIII').click();
     // find and click the link for Pokemon listing (using xpath locator)
     await page.locator('//html/body/div/div[2]/main/p[5]/a[1]').click();
-    // expect that the title of the page includes "Pokémon - Generation VIII Pokémon"
+    // expect that the title of the page is "Pokémon - Generation VIII Pokémon"
     await expect(page.locator('title')).toHaveText('Pokémon - Generation VIII Pokémon');
+  })
+
+  test('screenshot of POTW', async ({ page }) => {
+    // find and click the first image in the right sidebar (POTW image)
+    await page.locator('.tbarpic').first().click()
+    // take a full page screenshot of the current POTW
+    await page.screenshot({ path: 'potw.png', fullPage: true });
+    // assert that we are on the POTW page
+    await expect(page.locator('title')).toContainText('Pokémon of the Week');
   })
 })
